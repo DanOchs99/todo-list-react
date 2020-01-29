0daadaf6b1eedb802966c6313bfb84b25b4d7c5e
Dan Ochs <dochs@ochsholdings.com>
2020-01-28 22:17:38 -0600

    completed tasklist

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
         super(props)
         this.state = {
           taskName: '',
-          tasks: []
+          tasksPending: [],
+          tasksCompleted: []
         }
     }
 
@@ -20,17 +21,42 @@ class App extends Component {
 
     handleAddTask = () => {
         this.setState({
-            tasks: this.state.tasks.concat(this.state.taskName)
+            tasksPending: this.state.tasksPending.concat(this.state.taskName)
         })
+        document.getElementById('textBox').value = '';
     }
     
+    onCheckPending = (selection) => {
+        const moved = this.state.tasksPending.filter(task => task==this.state.tasksPending[selection])
+        const remaining = this.state.tasksPending.filter(task => task!=this.state.tasksPending[selection])
+        const completed = this.state.tasksCompleted.concat(moved)
+        this.setState({tasksPending: remaining, tasksCompleted: completed}) 
+    }
+
+    onCheckCompleted = (selection) => {
+        const moved = this.state.tasksCompleted.filter(task => task==this.state.tasksCompleted[selection])
+        const remaining = this.state.tasksCompleted.filter(task => task!=this.state.tasksCompleted[selection])
+        const pending = this.state.tasksPending.concat(moved)
+        this.setState({tasksPending: pending, tasksCompleted: remaining})
+    }
+    
+    onDeletePending = (selection) => {
+        const remaining = this.state.tasksPending.filter(task => task!=this.state.tasksPending[selection])
+        this.setState({tasksPending: remaining})
+    }
+
+    onDeleteCompleted = (selection) => {
+        const remaining = this.state.tasksCompleted.filter(task => task!=this.state.tasksCompleted[selection])
+        this.setState({tasksCompleted: remaining})
+    }
+
     render() {
-      console.log(this.state.tasks.length)
       return (
           <div>
-            <input onChange={this.handleChange} type="text" />
+            <input onChange={this.handleChange} type="text" id="textBox"/>
             <button onClick={this.handleAddTask}>Add</button>
-            <Tasklist tasks={this.state.tasks} />
+            <Tasklist tasks={this.state.tasksPending} name="Pending Tasks" onCheck={this.onCheckPending}  onDelete={this.onDeletePending} />
+            <Tasklist tasks={this.state.tasksCompleted} name="Completed Tasks"  onCheck={this.onCheckCompleted}  onDelete={this.onDeleteCompleted} />
           </div>
       );
     }
--- src/components/Tasklist.js
+++ src/components/Tasklist.js
@@ -2,8 +2,38 @@ import React, {Component} from 'react';
 import './Tasklist.css';
 
 class Tasklist extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {item: 0}
+    }
+
+    handleCheck = (e) => {
+        const selection = e.target.id;
+        this.setState({item: selection}, () => {this.props.onCheck(selection)})
+    }
+
+    handleDelete = (e) => {
+        const selection = e.target.id;
+        this.setState({item: selection}, () => {this.props.onDelete(selection)})
+    }
+
     render() {
-        return (<div>{this.props.tasks}</div>)
+        let taskElements = [];
+        taskElements.push(<div key={this.props.name} className="listName">{this.props.name}</div>);
+        for (let i=0; i<this.props.tasks.length; i++) {
+            taskElements.push(<div key={i}>
+                                <div className="listItem">{this.props.tasks[i]}</div>
+                                <button id={i} onClick={this.handleCheck} >✓</button>
+                                <button id={i} onClick={this.handleDelete} >x</button>
+                              </div>
+            )
+        }
+        
+        return (
+            <div className="taskList">
+              {taskElements}
+            </div>
+        )
     }
 }
 
